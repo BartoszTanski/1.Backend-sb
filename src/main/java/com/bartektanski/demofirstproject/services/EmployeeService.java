574efa99ed8a8892c5da8ff17a3590725b9089e8
Bartosz Tanski <b.t4nsky@gmail.com>
2023-02-06 13:13:53 +0100

    get employyes api

--- src/main/java/com/bartektanski/demofirstproject/services/EmployeeService.java
+++ src/main/java/com/bartektanski/demofirstproject/services/EmployeeService.java
@@ -1,10 +1,14 @@
 package com.bartektanski.demofirstproject.services;
 
+import java.util.List;
+
 import com.bartektanski.demofirstproject.model.Employee;
 
 public interface EmployeeService {
 
 	Employee createEmployee(Employee employee);
+
+	List<Employee> getAllEmployees();
 
 		
 }
